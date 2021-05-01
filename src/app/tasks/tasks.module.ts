547cee8801bb535fbc1ec3b827f6a47c5920fa82
Eduardo Martins <edmartins_br@outlook.com>
2021-05-01 19:24:43 -0300

    Services

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task, TaskService } from './shared'; // imported by autoimport extension
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    TaskService
   ]
 })
 export class TasksModule { }
